6c32941adfa1b1f285467bbc6325eca2792e3f5d
Yash <yashatkulwar2000@gmail.com>
2022-09-14 17:56:30 +0530

    Added Welcome to Indian State Census Analyzer Problem

---
+++ main/java/com/bridgelabz/Welcome.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian State Census Analyser Problem");
+    }
+}
